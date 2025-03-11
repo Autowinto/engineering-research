4654e395edaef154078c0a4f8cd53e9b79004b5b
Magnus Christian Larsen <happymansi@hotmail.com>
2025-03-11 11:43:47 +0100

    feat: added base regex analysis project

---
+++ regex/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fmt::Debug;
+use std::fs;
+use std::string;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please enter a path!");
+        return;
+    }
+    let path = &args[1];
+    let file_string = fs::read_to_string(path).expect("Failed to read file with provided path");
+
+    let json: serde_json::Value = 
+        serde_json::from_str(&file_string).expect("JSON was not well-formatted");
+    
+}
